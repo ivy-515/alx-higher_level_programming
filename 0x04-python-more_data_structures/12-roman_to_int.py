4b3e3ad70d4b3f717e04ad69617ffe645d0aeef6
ivy-515 <ivy.am515@gmail.com>
2023-06-14 18:02:45 -0700

    check 10

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -11,10 +11,11 @@ def roman_to_int(roman_string):
             "M": 1000
     }
     num = 0
+    i = 0
     roman = roman_string
     if type(roman) is not str or len(roman) is 0:
         return 0
-    for i in range(len(roman)):
+    for i in range(i, len(roman)):
         if i < len(roman) - 1 and dic[roman[i]] < dic[roman[i + 1]]:
             num -= dic[roman[i]]
         else:
